0626a3620b8a0e417bca7644e8b39152fba3899e
Demar_Qiu <qlkeep_faith@163.com>
2021-04-27 23:25:53 +0800

    关于链表的练习复习

--- Test/List.cpp
+++ Test/List.cpp
@@ -48,7 +48,165 @@ void List::ClearList()
 
 }
 
+bool List:: GetElem(int i, Node* pNode) //获取第i个节点
+{
+	if (i < 0 || i >= m_iLength)
+	{
+		return false;
+	}
+	Node* currentNode = m_pList;
+	for (int k = 0; k <= i; k++)
+	{
+		currentNode = currentNode->next;
+	}
+	pNode->data = currentNode->data;
+	return true;
+}
 
+
+int List::LocateElem(Node* pNode)  //节点定位
+{
+	Node* currentNode = m_pList;
+	int count = 0;
+	while (currentNode->next != NULL)
+	{
+		currentNode = currentNode->next;
+		if (currentNode->data == pNode->data)
+		{
+			return count;
+		}
+		count++;
+	}
+	return -1;
+
+}
+
+bool List:: ListInsertHead(Node* pNode)  //从头插入节点
+{
+	Node* temp = m_pList->next;
+	Node *newNode= new Node;
+	if (newNode == NULL)
+	{
+		return false;
+	}
+
+	newNode->data = pNode->data;
+	newNode->next = temp;
+	m_pList->next = newNode;
+	m_iLength++;
+	return true;
+}
+
+bool List::ListInsertTail(Node* pNode)
+{
+	Node* currentNode = m_pList;
+	while (currentNode->next != NULL)
+	{
+		currentNode = currentNode->next;
+	}
+	Node* newNode = new Node;
+	newNode->data = pNode->data;
+	currentNode->next = newNode;
+	newNode->next = NULL;
+	m_iLength++;
+	return true;
+
+}
+
+bool List::ListInsert(int i, Node* pNode)
+{
+	if (i<0 || i>m_iLength)
+	{
+		return false;
+	}
+	Node* currentNode = m_pList;
+	for (int k = 0; k < i; k++)
+	{
+		currentNode = currentNode->next;
+	}
+	Node* newNode = new Node;
+	if (newNode == NULL)
+	{
+		return false;
+	}
+	newNode->data = pNode->data;
+	newNode->next = currentNode->next;
+	currentNode->next = newNode;
+	return true;
+}
+
+bool List::ListDelete(int i, Node* pNode)
+{
+	if (i < 0 || i >= m_iLength)
+	{
+		return false;
+	}
+	Node* currentNode = m_pList;
+	Node* currentNodeBefore = NULL;
+	for (int k = 0; k <= i; k++)
+	{
+		currentNodeBefore = currentNode;
+		currentNode = currentNode->next;
+	}
+	currentNodeBefore->next = currentNode->next;
+	pNode->data = currentNode->data;
+	delete currentNode;
+	currentNode = NULL;
+	m_iLength--;
+}
+
+bool List::PriorElem(Node* pCurrentNode, Node* pPreNode)
+{
+	Node* currentNode = m_pList;
+	Node* tempNode = NULL;
+	while (currentNode->next != NULL)
+	{
+		tempNode = currentNode;
+		currentNode = currentNode->next;
+		if (currentNode->data == pCurrentNode->data)
+		{
+			if (tempNode == m_pList)
+			{
+				return false;
+			}
+			pPreNode->data = tempNode->data;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+bool List::NextElem(Node* pCurrentNode, Node* pNextNode)//找当前节点的后继节点
+{
+	Node* currentNode = m_pList;
+	//Node* tempNode = NULL;
+	while (currentNode->next != NULL)
+	{
+		currentNode = currentNode->next;
+		if (currentNode->data == pCurrentNode->data)
+		{
+			if (pCurrentNode->next == NULL)
+			{
+				return false;   //若当前节点是最后一个节点
+			}
+			pNextNode->data = currentNode->next->data;   //赋值当前节点的后继节点的数据域
+			return true;
+		}
+	}
+	return false;
+}
+
+
+void List::ListTraverse()  //遍历
+{
+	Node* currentNode = m_pList;
+	while (currentNode->next != NULL)
+	{
+		currentNode = currentNode->next;
+		currentNode->printNode();
+	}
+}
 
 
 
--- Test/List.h
+++ Test/List.h
@@ -10,11 +10,19 @@ public:
 	void ClearList();
 	int ListLength();
 	bool ListEmpty();
-	bool GetElem(int i, Node* pNode);
+	bool GetElem(int i, Node* pNode);  //获取第i个位置的节点放到pNode
+	int LocateElem(Node* pNode);
 
+	bool ListInsertHead(Node* pNode);  //从头插入节点(将一个节点插入头节点后边)
+	bool ListInsertTail(Node* pNode);  //从尾部插入
 
+	bool ListInsert(int i, Node* pNode);
+	bool ListDelete(int i, Node* pNode);
 
+	bool PriorElem(Node* pCurrentNode, Node* pPreNode);
+	bool NextElem(Node* pCurrentNode, Node* pNextNode);
 
+	void ListTraverse();
 private:
 	Node* m_pList;
 	int m_iLength;
