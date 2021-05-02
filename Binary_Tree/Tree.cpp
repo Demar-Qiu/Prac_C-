c0900ad687250ce43ad9e5f9190ef276ced023cf
Demar_Qiu <qlkeep_faith@163.com>
2021-05-02 23:21:07 +0800

    关于二叉树的基本操作实现

---
+++ Binary_Tree/Tree.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include"Tree.h"
+using namespace std;
+
+
+
+Tree::Tree(int size)
+{
+	m_iSize = size;
+	m_pTree = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		m_pTree[i] = 0;
+	}
+}
+
+Tree::~Tree()
+{
+	delete[]m_pTree;
+	m_pTree = NULL;
+}
+
+
+int* Tree::SearchNode(int nodeIndex)
+{
+	if (nodeIndex < 0 || nodeIndex >= m_iSize)
+	{
+		return NULL;
+	}
+	if (m_pTree[nodeIndex] == 0)  //用0表示当前不存在结点
+	{
+		return NULL;
+	}
+	return &m_pTree[nodeIndex];
+}
+
+
+bool Tree::AddNode(int nodeIndex, int direction, int* pNode)
+{
+	if (nodeIndex < 0 || nodeIndex >= m_iSize)
+	{
+		return false;
+	}
+	if (m_pTree[nodeIndex] == 0)  //用0表示当前不存在结点
+	{
+		return false;
+	}
+
+	if (direction == 0)    // 表示往左边插入
+	{
+		if (nodeIndex * 2 + 1 >= m_iSize)
+		{
+			return false;
+		}
+		if (m_pTree[nodeIndex * 2 + 1] != 0)  //不为0表示此时有结点所以不能插入
+		{
+			return false;
+		}
+		m_pTree[nodeIndex * 2 + 1] = *pNode;  //插入左节点
+	}
+	if (direction == 1)    // 表示往右边插入
+	{
+		if (nodeIndex * 2 + 2 >= m_iSize)
+		{
+			return false;
+		}
+		if (m_pTree[nodeIndex * 2 + 2] != 0)  //不为0表示此时有结点所以不能插入
+		{
+			return false;
+		}
+		m_pTree[nodeIndex * 2 + 2] = *pNode;  //插入右节点
+	}
+	return true;
+}
+
+
+bool Tree::DeleteNode(int nodeIndex, int* pNode)
+{
+
+	if (nodeIndex < 0 || nodeIndex >= m_iSize)
+	{
+		return false;
+	}
+	if (m_pTree[nodeIndex] == 0)  //用0表示当前不存在结点
+	{
+		return false;
+	}
+	*pNode = m_pTree[nodeIndex];  //将待删除结点中的值取出了放入pNode
+	m_pTree[nodeIndex] = 0;      //表示结点被删除
+	return true;
+}
+
+
+void Tree::TreeTraverse()
+{
+	for (int i = 0; i < m_iSize; i++)
+	{
+		cout << m_pTree[i] << ",";
+	}
+}
--- Binary_Tree/Tree.h
+++ Binary_Tree/Tree.h
@@ -1,1 +1,17 @@
 #pragma once
+
+
+class Tree
+{
+public:
+	Tree(int size);    //创建树
+	~Tree();          //销毁树
+	int* SearchNode(int nodeIndex);  //根据索引寻找节点
+	bool AddNode(int nodeIndex, int direction, int* pNode);  //添加结点
+	bool DeleteNode(int nodeIndex, int* pNode);  //删除结点
+	void TreeTraverse();        //遍历结点
+
+private:
+	int* m_pTree;
+	int m_iSize;    //整个数组的大小
+};
